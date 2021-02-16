d314e4b9514775a8cbd7feb86da53f557fde191c
Joonas Mankisenmaa <joonasmankisenmaa@Joonass-MBP.wlan>
2021-02-16 11:03:00 +0200

    Api key poistettu

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 const ICON_URL = 'http://openweathermap.org/img/wn/';
-const API_KEY = '758d26c0086aa9400e82f83b9a7b31b0';
+const API_KEY = '';
 
 export default function Weather ({lat,lng}) {
   const [temp, setTemp] = useState(0);
